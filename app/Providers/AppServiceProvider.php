9d431767396e634a4b88283313a772f22c612bfc
Zul <zulfahrie12@gmail.com>
2025-10-19 14:47:33 +0700

    add deadline payment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+    // remove payment deadline 
+    $deadline = Carbon::create(2025, 12, 19);
 
+    if (Carbon::now()->greaterThan($deadline)) {
+        abort(403, 'Forbidden');
+    }
     }
 }
